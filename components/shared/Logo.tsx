37f5a6b35daa6ab545472ea1b61f9d2a69189fe9
amincxo <amincxo@gmail.com>
2025-05-17 06:29:14 -0700

    add font

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function Logo() {
   return (
-    <div className="mx-auto w-32 h-auto mb-12 " >
+    <div className="mx-auto w-32 h-auto mb-15 " >
         <Image src="/images/iranExchaneLogo.png" alt='لوگو سایت' width={500} height={500} />
     </div>
   )
